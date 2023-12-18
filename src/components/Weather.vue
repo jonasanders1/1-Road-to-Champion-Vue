c3b8e7b1f7b060e7452b6afbbc0a43c95202ffd9
Jonas Andersen <Jonas@anders1.com>
2023-12-18 01:52:32 +0100

    Weather app done!

--- src/components/Weather.vue
+++ src/components/Weather.vue
@@ -2,7 +2,7 @@
   <div class="container">
     <div class="header">
       <div class="input-container">
-        <input type="text" v-model="city">
+        <input type="text" v-model="city" placeholder="Oslo...">
         <button @click="fetchWeatherData(city)">
           Search
         </button>
@@ -10,50 +10,56 @@
       </div>
     </div>
 
-    <div class="weather-content" v-if="weather.name">
-
-      <div class="city">
-        <h2>City</h2>
-        <p>City: {{ weather.name }}</p>
-        <p>Country code: {{ weather.countryCode }}</p>
+    <div class="card" v-if="weather.name">
+      <div class="card_header">
+        <div class="card_header-city">{{ weather.name }},</div>
+        <div class="card_header-country">{{ weather.countryCode }}</div>
+        <img :src="`http://openweathermap.org/img/wn/10d.png`" alt="Weather Icon">
       </div>
-      <div class="info-container">
+      <div class="content">
 
-        <div class="box">
-          <h2>Weather info</h2>
-          <p>Weather: {{ weather.weatherType }}</p>
-          <p>Description: {{ weather.weatherDescription }}</p>
-          <img :src="`http://openweathermap.org/img/wn/${weather.icon}.png`" alt="Weather Icon">
+        <div class="main">
 
-        </div>
-        <div class="box">
-          <h2>Main</h2>
-          <p>Temperature: {{ weather.temperature }}°C</p>
-          <div v-if="weather.sunrise && weather.sunrise">
-            <p>Min temp: {{ weather.temp_min }}°C</p>
-            <p>Max temp: {{ weather.temp_max }}°C</p>
+          <div class="description">
+            <p>{{ weather.weatherType }}</p>
+            <p>{{ weather.weatherDescription }}</p>
           </div>
-          <p>Pressure: {{ weather.pressure }}</p>
-          <p>Humidity: {{ weather.humidity }}</p>
-        </div>
+          <div class="description">
+            <p>Sunrise</p>
+            <p>{{ formatTime(weather.sunrise) }}</p>
+          </div>
+          <div class="description">
+            <p>Sunset</p>
+            <p>{{ formatTime(weather.sunset) }}</p>
+          </div>
 
+          <div class="temperature-container">
+            <div class="current-temp">
+              <p>{{ weather.temperature }}°C</p>
+            </div>
+          </div>
 
-        <div class="box">
-          <h2>Wind</h2>
-          <p>Wind speed: {{ weather.windSpeed }}</p>
-          <p>Wind deg: {{ weather.windSpeedDeg }}</p>
-        </div>
-        <div class="box">
-          <h2>Sunrise / sunset</h2>
-          <p>Sunrise: {{ formatTime(weather.sunrise) }} </p>
-          <p>Sunset: {{ formatTime(weather.sunset) }} </p>
+          <div class="unit-container">
+            <div class="unit">
+              <p>Pressure</p>
+              <p>{{ weather.pressure }}</p>
+            </div>
+            <div class="unit">
+              <p>Humidity</p>
+              <p>{{ weather.humidity }}%</p>
+            </div>
+            <div class="unit">
+              <p>Wind speed</p>
+              <p>{{ weather.windSpeed }} m/s</p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
-    <div v-else style="flex: 3; width: 100%; justify-content: center; align-items: center;">
-
+    <div v-else style="flex: 1; display: flex; justify-content: center;
+    align-items: center;">
+      <P style="font-size: 2rem; color: white ;">Enter a city....</P>
     </div>
-
   </div>
 </template>
 
@@ -127,16 +133,16 @@ export default {
   height: 100vh;
   display: flex;
   flex-direction: column;
+  background: linear-gradient(-35deg, rgb(0, 179, 255), rgb(45, 136, 241));
 }
 
 .header {
-  background: url("../assets/sky.png");
   background-size: cover;
   background-repeat: no-repeat;
   display: flex;
-  flex: 1;
-  justify-content: center;
   align-items: center;
+  height: 15vh;
+  justify-content: center;
 }
 
 .input-container {
@@ -149,18 +155,27 @@ input {
   padding: 5px 10px;
   font-size: 1.1rem;
   border-radius: 20px;
-  color: rgb(46, 112, 226);
+  color: rgb(57, 57, 57);
   font-weight: 500;
 }
 
 button {
+  font-weight: 500;
+  font-size: 1.1rem;
   border: none;
   display: flex;
   align-items: center;
   justify-content: center;
   border-radius: 20px;
   color: white;
-  background-color: rgb(46, 112, 226);
+  opacity: .5;
+  background: linear-gradient(45deg, rgb(2, 120, 255), rgb(3, 121, 255));
+  transition: all ease 250ms;
+}
+
+button:focus,
+:hover {
+  opacity: 1;
 }
 
 input,
@@ -168,22 +183,88 @@ button {
   outline: none;
 }
 
-.weather-content {
-  border: 1px solid blue;
-  background-color: rgb(245, 245, 245);
-  flex: 3;
+.card {
+  margin: 100px auto 0 auto;
+  background-color: rgba(0, 0, 0, 0.5);
+  border-radius: 15px;
+  padding: 20px;
+  height: 350px;
+  width: 300px;
+  box-shadow: 5px 5px 8px rgba(0, 0, 0, 0.2);
+  color: white;
 }
+
+.card_header {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  font-size: 1.8rem;
+  margin: 10px 0px;
+}
+
+.card_header-country {
+  margin-left: 5px;
+  color: rgb(70, 181, 255);
+}
+
+.description {
+  display: flex;
+  justify-content: space-between;
+  border-bottom: 1px solid rgb(202, 202, 202);
+}
+
+.description p {
+  margin: 0px;
+  padding: 10px;
+}
+
+.temperature-container {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: 30px 0px;
+}
+
+.current-temp {
+  font-size: 3rem;
+}
+
+.main {
+  display: flex;
+  flex-direction: column;
+  height: 230px;
+  justify-content: space-between;
+}
+
+.unit-container {
+  display: flex;
+  justify-content: space-between;
+  margin-top: auto;
+}
+
+.unit {
+  display: flex;
+  gap: 2px;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+
+p {
+  padding: 0%;
+  margin: 0%;
+}
+
+img {
+  width: 65px;
+  aspect-ratio: 1;
+}
+
 .info-container {
   padding: 20px;
   display: grid;
   grid-template-columns: 1fr 1fr;
   gap: 10px;
 
-}
-
-.box {
-  padding: 20px;
-  background-color: orange;
-  border-radius: 20px;
-}
-</style>
+}</style>
